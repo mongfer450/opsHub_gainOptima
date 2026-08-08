a36a60b8edfa2b87076db1ad56ef61bba954d947
Mongfer <90517849+mongfer450@users.noreply.github.com>
2026-08-08 10:26:11 +0700

    Update target constants for club and PT

--- src/config/constants.js
+++ src/config/constants.js
@@ -7,5 +7,5 @@ export const REVENUE_SHEET_ID = "11JY-u1njafkk_zIQSX4N-FQIRvvXGoTwR9MWkNkT3s4";
 export const ATTENDANCE_SHEET_ID = "1xH5kKeXAqNaEZzheWAFZEKdQHbsMi55AipuoTkn_PoY";
 export const ATTENDANCE_SHEET_TAB = "การตอบแบบฟอร์ม 1";
 
-export const CLUB_TARGET = 1000000;
-export const PT_TARGET = 500000;
+export const CLUB_TARGET = 1500000;
+export const PT_TARGET = 750000;
